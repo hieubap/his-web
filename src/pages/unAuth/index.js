2f7d45eb5e71fe35c57e01299bd39811ee7bc9b0
huyen.dt <huyen.dt@isofh.com>
2021-01-06 10:34:21 +0700

    edit project

--- src/pages/unAuth/index.js
+++ src/pages/unAuth/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Icon } from 'antd';
 import { Main } from './styled';
-
+import { WarningOutlined } from '@ant-design/icons';
 const UnAuth = () => {
   return (
     <Main>
-      <Icon type="warning" style={{ fontSize: 240, color: '#faad14' }} />
+      <WarningOutlined style={{ fontSize: 240, color: '#faad14' }} />
       <span className={'un-auth-mess'}>{'Bạn không được phép truy cập màn hình này!'}</span>
     </Main>
   )
