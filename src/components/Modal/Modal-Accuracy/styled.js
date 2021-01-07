379cb27e692bc5aad30b1a2496744ebc7cf62fdf
ADMIN\Admin <nam.nh@isofhcare.com>
2021-01-07 14:01:50 +0700

    fix done all

--- src/components/Modal/Modal-Accuracy/styled.js
+++ src/components/Modal/Modal-Accuracy/styled.js
@@ -4,7 +4,10 @@ import { displayFlex, button } from "components/mixin";
 
 export const Main = styled(Modal)`
     font-family: "Nunito Sans" !important;
-    width: 700px!important;  
+    width: 700px!important;
+    p{
+        margin: 0;
+    }  
     .ant-modal-footer{
         display: none;
      }
@@ -29,11 +32,49 @@ export const Main = styled(Modal)`
             line-height: 22px;
             color: #42526E;
         }
-        .main{
-            width:100%;
-            height:325px;
+        .content{
+            margin-top: 6px;
+            position: relative;
+            width:605px;
+            height: 285px;
+            box-shadow: 0px 4px 10px 2px rgba(0, 0, 0, 0.2);
+            z-index:2;
+            padding: 24px 0 0 24px;
+            &::before{
+                position: absolute;
+                content:"";
+                width:584px;
+                top: 10px;
+                left: 10px;
+                height: 266px;
+                border: 2px solid #0291E1;
+            }
+            h6{
+                margin-bottom:0;
+                font-weight: normal;
+                font-size: 14px;
+                line-height: 18px;
+                color: #000000;
+                padding: 3px 0;
+            }
+            p{
+                font-size: 14px;
+                line-height: 18px;
+                color: #000000;
+                padding: 3px 0;
+            }
+            .line{
+                width: 92%;
+                height: 1px;
+                background-color: black;
+                position: absolute;
+                top: 104px;
+                left: 24px;
+                opacity: 0.2;
+            }
         }
         .button-bottom{
+            margin-top: 32px;
             margin-bottom:10px;
             ${displayFlex("space-between","center")}
             .btn{
